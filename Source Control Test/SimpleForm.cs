5a4cc4984c3c044887ebdecd04d2180761629a34
candura <gordon.hoops@candura.com>
2014-09-12 15:51:45 -0400

    Development Add 1

--- Source Control Test/SimpleForm.cs
+++ Source Control Test/SimpleForm.cs
@@ -36,6 +36,11 @@ namespace Source_Control_Test
                                 "Information", 
                                 MessageBoxButtons.OK, 
                                 MessageBoxIcon.Information );
+
+            MessageBox.Show(    "Development Add 1", 
+                                "Information", 
+                                MessageBoxButtons.OK, 
+                                MessageBoxIcon.Information );
         }
         //
     }
