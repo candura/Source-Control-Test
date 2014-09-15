c3934ad9c5d3402f70a2ea9230be3548cd8ba72a
candura <gordon.hoops@candura.com>
2014-09-15 10:15:35 -0400

    Development Add 3

--- Source Control Test/Messages.cs
+++ Source Control Test/Messages.cs
@@ -25,6 +25,11 @@ namespace Source_Control_Test
                                 "Information", 
                                 MessageBoxButtons.OK, 
                                 MessageBoxIcon.Information );
+
+            MessageBox.Show(    "Development Add 3", 
+                                "Information", 
+                                MessageBoxButtons.OK, 
+                                MessageBoxIcon.Information );
         }
     }
 }
