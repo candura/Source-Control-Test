bfc7621587904e7b606d20b37ac15e193950b4d2
candura <gordon.hoops@candura.com>
2014-09-15 09:45:11 -0400

    StableRelease Add 1

--- Source Control Test/Messages.cs
+++ Source Control Test/Messages.cs
@@ -15,6 +15,11 @@ namespace Source_Control_Test
                                 "Information", 
                                 MessageBoxButtons.OK, 
                                 MessageBoxIcon.Information );
+
+            MessageBox.Show(    "StableRelease Add 1", 
+                                "Information", 
+                                MessageBoxButtons.OK, 
+                                MessageBoxIcon.Information );
         }
     }
 }
