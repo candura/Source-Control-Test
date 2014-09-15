7c39f79c3ab09da62c0d761d1b85c96b1a19ccf3
candura <gordon.hoops@candura.com>
2014-09-15 09:13:15 -0400

    Development Add 5

--- Source Control Test/SimpleForm.cs
+++ Source Control Test/SimpleForm.cs
@@ -47,6 +47,11 @@ namespace Source_Control_Test
                                 MessageBoxButtons.OK, 
                                 MessageBoxIcon.Information );
 
+            MessageBox.Show(    "Development Add 5", 
+                                "Information", 
+                                MessageBoxButtons.OK, 
+                                MessageBoxIcon.Information );
+
             MessageBox.Show(    "StableRelease Add 1", 
                                 "Information", 
                                 MessageBoxButtons.OK, 
