0fdd8870863ca9490fc30a8ce6266ef8059756c8
candura <gordon.hoops@candura.com>
2014-09-15 10:22:31 -0400

    Development Add 8

--- Source Control Test/SimpleForm.cs
+++ Source Control Test/SimpleForm.cs
@@ -63,6 +63,11 @@ namespace Source_Control_Test
                                 MessageBoxButtons.OK, 
                                 MessageBoxIcon.Information );
 
+            MessageBox.Show(    "Development Add 8", 
+                                "Information", 
+                                MessageBoxButtons.OK, 
+                                MessageBoxIcon.Information );
+
             MessageBox.Show(    "StableRelease Add 1", 
                                 "Information", 
                                 MessageBoxButtons.OK, 
