abcb54860e4a408f99442eaf9dd5830b91945239
candura <gordon.hoops@candura.com>
2014-09-15 10:20:33 -0400

    Development Add 6

--- Source Control Test/Messages.cs
+++ Source Control Test/Messages.cs
@@ -41,6 +41,11 @@ namespace Source_Control_Test
                                 MessageBoxButtons.OK, 
                                 MessageBoxIcon.Information );
 
+            MessageBox.Show(    "Development Add 6", 
+                                "Information", 
+                                MessageBoxButtons.OK, 
+                                MessageBoxIcon.Information );
+
             MessageBox.Show(    "StableRelease Add 1", 
                                 "Information", 
                                 MessageBoxButtons.OK, 
