3feaadd086abe8b08d121992ca2b2f29a3b7462d
candura <gordon.hoops@candura.com>
2014-09-19 09:32:45 -0400

    Removed messages.

--- Source Control Test/Messages.cs
+++ Source Control Test/Messages.cs
@@ -15,11 +15,6 @@ namespace Source_Control_Test
                                 "Information", 
                                 MessageBoxButtons.OK, 
                                 MessageBoxIcon.Information );
-
-            MessageBox.Show(    "Development Add 1", 
-                                "Information", 
-                                MessageBoxButtons.OK, 
-                                MessageBoxIcon.Information );
         }
     }
 }
